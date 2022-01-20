f5c3b8c52026da6712ba7f8fb1b64854d004d964
Leticia Calixto <leticiamoreirac@hotmail.com>
2022-01-20 13:00:03 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/TSTCORP/s3-bucket/aws"
   version = "2.2.0"
   acl    = "private"
- bucket = "my-s3-bucket"
  bucket_prefix = "${var.prefix}"
 
   versioning = {
